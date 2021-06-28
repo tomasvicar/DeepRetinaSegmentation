3426e586e5bded32bbf4f5b5626c6128eb03e950
tomasvicar <tomasvicar@gmail.com>
2021-06-28 13:40:28 +0200

    fgdfgf

--- main_drive_opt.py
+++ main_drive_opt.py
@@ -53,6 +53,50 @@ def train_one_model(config,it):
     accs,aucs,dices,tps,fps,fns,tns = test_fcn_vessels('../' + config.method + '/' + database_name + str(it), config, model_name, tmp_test)
     
     return np.mean(aucs)
+
+
+
+# def train_one_model(config,it):
+    
+#     data_split = DataSpliter.split_data(data_type=DataSpliter.DATA_TYPE_VESSELS,seed=0*100)
+#     data_split['database_names'] = ['drive']
+    
+#     if os.path.isdir(config.model_save_dir):
+#                 rmtree(config.model_save_dir) 
+                
+                
+#     if not os.path.isdir(config.best_models_dir):
+#         os.mkdir(config.best_models_dir)
+        
+#     if not os.path.isdir(config.model_save_dir):
+#         os.mkdir(config.model_save_dir)
+        
+#     if not os.path.isdir(config.results_folder):
+#         os.mkdir(config.results_folder)
+        
+#     database_name = data_split['database_names'][0]    
+        
+#     config.method = 'segmentation_pretrain' + database_name + str(it)
+#     config.model_name_load = None
+#     universal_model_name = train(config,data_train=data_split['train'],data_valid=data_split['valid'])
+    
+
+#     tmp_train = DataSpliter.filter_database(data_split['train'],database_name)
+#     tmp_valid = DataSpliter.filter_database(data_split['valid'],database_name)
+#     tmp_test = DataSpliter.filter_database(data_split['test'],database_name)
+
+#     # init_model = None
+#     config.method = 'segmentation_retrain' + database_name + str(it)
+#     config.model_name_load = universal_model_name
+    
+    
+#     model_name = train(config,data_train=tmp_train,data_valid=tmp_valid)
+#     accs,aucs,dices,tps,fps,fns,tns = test_fcn_vessels('../' + config.method + '/' + database_name + str(it), config, model_name, tmp_test)
+    
+#     return np.mean(aucs)
+
+
+
     
 
 
