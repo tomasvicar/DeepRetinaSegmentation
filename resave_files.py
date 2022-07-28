8e921ea928ff2f1a1b2a86b1d6643bb76b93e948
Tomas Vicar <vicar@vutbr.cz>
2022-07-28 16:47:19 +0200

    dfsdf

--- resave_files.py
+++ resave_files.py
@@ -1,7 +1,54 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Jul 28 15:53:08 2022
+from glob import glob
+import os
+import shutil
 
-@author: vicar
-"""
+
+# input_path = '../spocitane_trasformace_all/Sada_01'
+# output_path = '../final_multimodal_results/Sada_01'
+
+input_path = r'C:\Data\Vicar\retina_vessels_segmentation\Sada_01\Sada_01'
+output_path = r'C:\Data\Vicar\retina_vessels_segmentation\trasfromed_segmentation\Sada_01'
+
+
+sub_path = '/ImageAnalysis/VesselsSeg/'
+
+
+to_resaves = []
+with_names = []
+
+to_resaves.append('/**/*_avg_registered_nocorrupted.png')
+with_names.append(True)
+
+to_resaves.append('/**/*_disk_detection.png')
+with_names.append(True)
+
+to_resaves.append('/**/first_try_best/init_trasform_parameters.json')
+with_names.append(False)
+
+to_resaves.append('/**/first_try_best/transformation.npy'  )
+with_names.append(False)
+       
+
+for to_resave, with_name in zip(to_resaves,with_names):
+
+    filenames = glob(input_path + to_resave,recursive=True)
+    
+    
+    for file_num, filename in enumerate(filenames):
+        
+        
+        filename_save =  filename.replace(input_path,'')
+        
+        filename_save = os.path.normpath(filename_save)
+        
+        if not with_name:
+            filename_save = output_path + os.sep + filename_save.split(os.sep)[1] + sub_path + filename_save.split(os.sep)[1] + '_' + os.path.split(filename_save)[1]
+        else:
+            filename_save = output_path + os.sep + filename_save.split(os.sep)[1] + sub_path + os.path.split(filename_save)[1]
+        
+        if not os.path.exists(os.path.split(filename_save)[0]):
+            os.makedirs(os.path.split(filename_save)[0]) 
+        
+        shutil.copyfile(filename, filename_save)
+
 
